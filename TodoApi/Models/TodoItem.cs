888cda9be990ed6d771c7d0355ce26266e347bb1
Aleks0308 <aleksandrs.asmikovics@gmail.com>
2024-11-10 16:37:04 +0000

    Initial structure and scaffolding of Todo List API

---
+++ TodoApi/Models/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿namespace TodoApi.Models;
+
+public class TodoItem
+{
+    public long Id { get; set; }
+    public string? Title { get; set; }
+    public string? Body{ get; set; }
+    public bool IsComplete { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public DateTime? CompletedAt { get; set; }
+}
